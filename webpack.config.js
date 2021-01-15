4ab6d1462eecb42b50a893c94206ae660666edbd
Dale Rushin <dale.rushin@bunches.co.uk>
2021-01-15 15:10:30 +0000

    Adds Webpack to build minified build

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve('build'),
+    filename: 'index.js',
+    libraryTarget: 'commonjs2'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  externals: {
+    react: 'commonjs react',
+    'react-dom': 'commonjs react-dom',
+  }
+}
